4a6e75c1af3e73fc42a10dfa24791f4fb57ee355
chris butler <chrisbutler@chriss-MacBook-Pro-2.local>
2025-01-27 21:43:27 +0000

    shapefile creation, and geopandas install

--- scripts/shapefile_create.py
+++ scripts/shapefile_create.py
@@ -9,6 +9,8 @@ Created on Sun Jan 26 17:53:41 2025
 import pandas as pd
 import pytest
 import os 
+import geopandas as gpd
+from shapely.geometry import Point
 
 #import the file 
 
@@ -17,7 +19,7 @@ def test_files():
 
 
 shapefile = pd.read_csv('../data/munrotab_v8.0.1.csv', encoding='latin1')
-#shapefile.head()
+
 
 
 #data clean 
@@ -31,3 +33,22 @@ def test_munro_count():
     assert shapefile.shape[0] == 282, 'munro count is not coming back as 282?'
     
 shapefile = shapefile.drop('2021', axis = 1)
+
+#export as a shapefile 
+
+geometry = [Point(xy) for xy in zip(shapefile['xcoord'], shapefile['ycoord'])]
+
+shapefile = gpd.GeoDataFrame(shapefile, geometry = geometry)
+
+def test_shapefile():
+    assert isinstance(shapefile, gpd.GeoDataFrame), 'file was not correctly converted to a GeoDataFrame Object'
+    
+#align to correct CRS
+shapefile = shapefile.set_crs(epsg=3857)
+
+def test_crs():
+    assert shapefile.crs == 'EPSG:3857', 'file could not be converted to the correct coordinate reference system (3857)'
+    
+#save file 
+shapefile.to_file('../outputs/munro.shp') 
+   
